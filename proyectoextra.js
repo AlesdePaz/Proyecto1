831d8d7d57716c952771fe1a8062af9d092312f5
Ulises de Paz <alesdepaz16@gmail.com>
2021-02-18 16:24:02 -0600

    se agrego el cargo por propiedad y el recargo por salario

--- proyectoextra.js
+++ proyectoextra.js
@@ -1,4 +1,7 @@
-﻿//Variables utiles 
+﻿//noprotect
+while(transaccion != "SALIR" && transaccion != "salir" ) {
+  
+//Variables utiles 
 //Precio base de la cotización, en quetzales, lo puede cambiar
 var precio_base = 2000
 
@@ -11,18 +14,29 @@ var casado_18 = 0.1 // 10%
 var casado_25 = 0.2 // 20%
 var casado_50 = 0.3 // 30%
 
+//hijos
 var recargo_hijos = 0.2 // 20%
+var cantidad_hijos = 0
+var cantidad_hijos_numero = 0
+var recargo_hijos_total = 0
 
 //Recargo
 var recargo = 0
 var recargo_total = 0
-var recargo_hijos_total = 0
+
+// recargo por propiedades
+var propiedades = 0
+var recargo_propiedades = 0.35 //35%
+var recargo_propiedades_total = 0
+
+// recargo por sueldo 5% sobre el salario del asegurado
+ var sueldo = 0
+var recargo_salario = 0.05 //5%
+var recargo_salario_total = 0
 
 //Precio final 
 var precio_final = 0
 var transaccion
-
-while(transaccion != "SALIR" && transaccion != "salir" ) { 
 
 //Mensajes de alerta para ingresar datos 
 var nombre = prompt("Ingrese su nombre, por favor")
@@ -30,7 +44,7 @@ var edad = prompt("¿Cuantos años tiene? Ingrese solamente números ")
 if(edad>=18){
 var casado = prompt("¿Está casado actualmente?")
 //Comprobamos la edad del cónyuge, solamente si se está casado/a
-var edad_conyuge
+var edad_conyuge = 0
 if("SI" == casado.toUpperCase()){
   edad_conyuge = prompt("¿Que edad tiene su esposo/a?")
 }
@@ -45,13 +59,13 @@ if("SI" == casado.toUpperCase()){
 var hijos = prompt("¿Tiene hijos o hijas?")
 //Comprobamos la cantidad de hijos solamente si los tienen
 if("SI" == hijos.toUpperCase()){
-  var cantidad_hijos = prompt("¿cuantos hij@s tiene?")
+  cantidad_hijos = prompt("¿cuantos hij@s tiene?")
 }
 /**
  * 1. convierta la cantidad de hijos a numero
  */
 if("SI" == hijos.toUpperCase()){
-  var cantidad_hijos_numero = parseInt(cantidad_hijos)
+  cantidad_hijos_numero = parseInt(cantidad_hijos)
   }
 //Aquí debe calcular el recargo total basado en las respuestas ingresadas
 
@@ -76,7 +90,7 @@ else if(edad_numero>=50){
 /** 
  * 2. Recargo por la edad del conyuge
  */
-var recargo_conyuge
+var recargo_conyuge = 0
 if(edad_conyuge_numero>=18 && edad_conyuge_numero<25){ 
  //Calculamos el recargo en base a la edad del conyuge
   recargo_conyuge = precio_base * casado_18
@@ -99,8 +113,22 @@ for (var i = 1; i <= cantidad_hijos_numero; i++){
  recargo_hijos_total = precio_base * recargo_hijos
   recargo_total = recargo_total + recargo_hijos_total
 }
+//extras
+  propiedades = parseInt(prompt ("cuantas propiedades tiene? Ingrese solamente números"))
+ //se realiza el recargo por propiedades
+  for (var i = 1; i <= propiedades; i++){
+//instrucciones a ejecutar
+ recargo_propiedades_total = precio_base * recargo_propiedades
+  recargo_total = recargo_total + recargo_propiedades_total
+}
 
+  sueldo = parseInt(prompt ("cual es su salario mensual? Ingrese solamente números"))
+ /*se realiza el recargo por sueldo
+ instrucciones a ejecutar*/
+ recargo_salario_total = sueldo * recargo_salario
+  recargo_total = recargo_total + recargo_salario_total
 
+  
 precio_final = precio_base + recargo_total
 //Resultado
 alert ("Para el asegurado "+nombre)
